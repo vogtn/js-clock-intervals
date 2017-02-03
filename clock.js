2c3798cecb1bb6de319348efaf0dfbe0a25cd80c
vogtn <nicolasjvogt@gmail.com>
2017-02-02 22:43:49 -0800

    real solution

--- clock.js
+++ clock.js
@@ -1,2 +1,46 @@
 var now = new Date();
-console.log(now.getSeconds());
+
+var rotateThisSecond = now.getSeconds();
+var rotateThisMinute= now.getMinutes() * 6;
+
+var rotateThisHour = null;
+//handling 24 hour => 12
+if(now.getHours() > 12){
+  rotateThisHour = (now.getHours() -2) * 15;
+}else{
+  rotateThisHour = now.getHours() * 30;
+}
+
+console.log(now.getHours())
+
+console.log(rotateThisHour);
+
+var initial = function(){
+  document.getElementById("second").style.transform = "rotate(" + rotateThisSecond +"deg)";
+  document.getElementById("minute").style.transform = "rotate(" + rotateThisMinute +"deg)";
+  document.getElementById("hour").style.transform = "rotate(" + rotateThisHour +"deg)";
+}
+var start = setInterval(initial, 5);
+
+var moveSecond = function(){
+  rotateThisSecond += 6;
+  document.getElementById("second").style.transform = "rotate(" + rotateThisSecond +"deg)";
+}
+
+
+
+var moveMinute = function(){
+  rotateThisMinute += 6;
+  document.getElementById("minute").style.transform = "rotate(" + rotateThisMinute +"deg)";
+}
+
+
+
+var moveHour = function(){
+  rotateThisHour += 30;
+  document.getElementById("hour").style.transform = "rotate(" + rotateThisHour +"deg)";
+}
+
+var secondInterval = setInterval(moveSecond, 1000);
+var minuteInterval = setInterval(moveMinute, (1000 * 60));
+var hourInterval = setInterval(moveHour, (1000 * 60 * 60));
